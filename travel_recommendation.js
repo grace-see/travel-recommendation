3b7eec223f4f99ceb6a512d7dee6f13628e8bfd0
grace-see <171896811+grace-see@users.noreply.github.com>
2026-07-20 15:06:13 -0500

    Added search results page and clear button functionality

--- travel_recommendation.js
+++ travel_recommendation.js
@@ -1,3 +1,9 @@
+function clearContent(element) {
+    while (element.firstChild) {
+        element.removeChild(element.firstChild);
+    }
+}
+
 async function fetchSearchResults() {
     const searchContent = document.querySelector("#searchinput").value.toLowerCase().trim();
     const response = await fetch("./travel_recommendation_api.json")
@@ -8,19 +14,137 @@ async function fetchSearchResults() {
             return response.json();
         })
         .then(data => {
-            //fetching: beaches, countries, or temples
-                //each object will be its own entry
-                //except for with countries, where we'll have to loop through the cities instead
-            //we display cards for each of the entries that we get
-            if (searchContent.match(/beach(es)?/))
-                console.log("BEACH");
-            else if (searchContent.match(/temple(s)?/))
-                console.log("TEMPLE");
-            else if (searchContent.match(/countr(y|ies)/))
-                console.log("COUNTRY");
+            if (searchContent.match(/beach(es)?/)) {
+                const mainDiv = document.querySelector(".main-div");
+                mainDiv.classList.add("search");
+                if (mainDiv.classList.contains("contact-content"))
+                    mainDiv.classList.remove("contact-content");
+                //clear content of current div
+                clearContent(mainDiv);
+                //create header
+                const resultsHeader = document.createElement("h2");
+                resultsHeader.textContent = "Search Results";
+                resultsHeader.classList.add("results-header");
+                mainDiv.appendChild(resultsHeader);
+                //create container to hold search results
+                const resultsContainer = document.createElement("div");
+                resultsContainer.classList.add("results-container");
+                //create search results
+                const searchData = data.beaches;
+                searchData.forEach((entry) => {
+                    //set up entry title
+                    const entryName = document.createElement("h3");
+                    entryName.textContent = entry.name;
+                    //set up entry description
+                    const entryDescription = document.createElement("p");
+                    entryDescription.textContent = entry.description;
+                    //set up entry image
+                    const entryImg = document.createElement("img");
+                    entryImg.src = entry.imageUrl;
+                    //set up container for result entry
+                    const entryContainer = document.createElement("div");
+                    entryContainer.classList.add("entry-container");
+                    entryContainer.appendChild(entryImg);
+                    entryContainer.appendChild(entryName);
+                    entryContainer.appendChild(entryDescription);
+                    //append final result entry to the results container
+                    resultsContainer.appendChild(entryContainer);
+                });
+                //append to mainDiv
+                mainDiv.appendChild(resultsContainer);
+            }
+            else if (searchContent.match(/temple(s)?/)) {
+                const mainDiv = document.querySelector(".main-div");
+                mainDiv.classList.add("search");
+                if (mainDiv.classList.contains("contact-content"))
+                    mainDiv.classList.remove("contact-content");
+                //clear content of current div
+                clearContent(mainDiv);
+                //create header
+                const resultsHeader = document.createElement("h2");
+                resultsHeader.textContent = "Search Results";
+                resultsHeader.classList.add("results-header");
+                mainDiv.appendChild(resultsHeader);
+                //create container to hold search results
+                const resultsContainer = document.createElement("div");
+                resultsContainer.classList.add("results-container");
+                //create search results
+                const searchData = data.temples;
+                searchData.forEach((entry) => {
+                    //set up entry title
+                    const entryName = document.createElement("h3");
+                    entryName.textContent = entry.name;
+                    //set up entry description
+                    const entryDescription = document.createElement("p");
+                    entryDescription.textContent = entry.description;
+                    //set up entry image
+                    const entryImg = document.createElement("img");
+                    entryImg.src = entry.imageUrl;
+                    //set up container for result entry
+                    const entryContainer = document.createElement("div");
+                    entryContainer.classList.add("entry-container");
+                    entryContainer.appendChild(entryImg);
+                    entryContainer.appendChild(entryName);
+                    entryContainer.appendChild(entryDescription);
+                    //append final result entry to the results container
+                    resultsContainer.appendChild(entryContainer);
+                });
+                //append to mainDiv
+                mainDiv.appendChild(resultsContainer);
+            }
+            else if (searchContent.match(/countr(y|ies)/)) {
+                const mainDiv = document.querySelector(".main-div");
+                mainDiv.classList.add("search");
+                if (mainDiv.classList.contains("contact-content"))
+                    mainDiv.classList.remove("contact-content");
+                //clear content of current div
+                clearContent(mainDiv);
+                //create header
+                const resultsHeader = document.createElement("h2");
+                resultsHeader.textContent = "Search Results";
+                resultsHeader.classList.add("results-header");
+                mainDiv.appendChild(resultsHeader);
+                //create container to hold search results
+                const resultsContainer = document.createElement("div");
+                resultsContainer.classList.add("results-container");
+                //create search results
+                const searchData = data.countries;
+                searchData.forEach((country) => {
+                    const cities = country.cities;
+                    cities.forEach((entry) => {
+                        //set up entry title
+                        const entryName = document.createElement("h3");
+                        entryName.textContent = entry.name;
+                        //set up entry description
+                        const entryDescription = document.createElement("p");
+                        entryDescription.textContent = entry.description;
+                        //set up entry image
+                        const entryImg = document.createElement("img");
+                        entryImg.src = entry.imageUrl;
+                        //set up container for result entry
+                        const entryContainer = document.createElement("div");
+                        entryContainer.classList.add("entry-container");
+                        entryContainer.appendChild(entryImg);
+                        entryContainer.appendChild(entryName);
+                        entryContainer.appendChild(entryDescription);
+                        //append final result entry to the results container
+                        resultsContainer.appendChild(entryContainer);
+                    });
+                });
+                //append to mainDiv
+                mainDiv.appendChild(resultsContainer);
+            }
         })
         .catch(error => console.error(error.message));
 }
 
+function clearSearch() {
+    const searchBar = document.querySelector("#searchinput");
+    searchBar.value = "";
+}
+
 searchBtn = document.querySelector("#searchBtn");
 searchBtn.addEventListener("click", fetchSearchResults);
+
+clearBtn = document.querySelector("#clearBtn");
+clearBtn.addEventListener("click", clearSearch);
